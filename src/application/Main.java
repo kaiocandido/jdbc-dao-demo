2d6b82d1752b2f2067a97f2f54d148ca3e4e5428
adm <kaio.candf@gmail.com>
2025-10-28 15:19:42 -0300

    entities de department

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,3 +1,7 @@
+package application;
+
+import model.entities.Department;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {

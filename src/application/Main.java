e180cd5fdcbe80f6c00220543491a8c5931270cf
adm <kaio.candf@gmail.com>
2025-10-28 15:35:03 -0300

    criado Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,22 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Department dp = new Department(1, "DEV");
+
+        //DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        Seller sl = new Seller(1, "kaio", "kaio.candf@gmail.com", new Date(), 7500.00, dp);
+
+        System.out.println(sl);
 
     }
 }
